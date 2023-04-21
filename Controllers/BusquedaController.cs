644f4955617b2c2303fbcea0783723b0b11530ee
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-04-21 13:17:53 -0600

    refactor: handle errors

--- Controllers/BusquedaController.cs
+++ Controllers/BusquedaController.cs
@@ -24,34 +24,53 @@ namespace DatosPacientes.Controllers
         }
 
         [HttpGet("paciente/{NoHistoriaClinica}")]
-        public async Task<List<PacienteCompletoDTO>> GetPersonas(string NoHistoriaClinica)
+        public async Task<ActionResult<List<PacienteCompletoDTO>>> GetPersonas(string NoHistoriaClinica)
         {
-            var query = _context.Pacientes
-            .Join(_context.Personas, p => p.Persona, per => per.Codigo, (p, per) => new { Paciente = p, Persona = per })
-            .Where(a => (a.Paciente.NoHistoriaClinica.Contains(NoHistoriaClinica) || NoHistoriaClinica == "-1"))
-            .OrderBy(a => a.Persona.Nombre1)
-            .Select(a => new PacienteCompletoDTO()
+            try
             {
-                Codigo = a.Paciente.Codigo,
-                Persona = a.Paciente.Persona,
-                Nombres = a.Persona.Nombre1 + " " + (a.Persona.Nombre2 ?? ""),
-                Apellidos = a.Persona.Apellido1 + " " + (a.Persona.Apellido2 ?? ""),
-                NoHistoriaClinica = a.Paciente.NoHistoriaClinica,
-                FechaNacimiento = a.Persona.FechaNacimiento,
-                Sexo = a.Persona.Sexo,
-                NombrePadre = a.Paciente.NombrePadre,
-                NombreMadre = a.Paciente.NombreMadre,
-                LugarNacimiento = a.Paciente.LugarNacimiento,
-                Archivo_Fisico = a.Paciente.ArchivoFisico,
-                Nombre_Resposable = a.Paciente.NombreResponsable,
-                Direccion_Responsable = a.Paciente.DireccionResponsable,
-                Telefono_Responsable = a.Paciente.TelefonoResponsable
 
-            });
+                if (string.IsNullOrWhiteSpace(NoHistoriaClinica))
+                {
+                    return BadRequest("NoHistoriaClinica no puede ser nolo o vacío");
+                }
 
-            return await query.ToListAsync();
+                var query = _context.Pacientes
+                .Join(_context.Personas, p => p.Persona, per => per.Codigo, (p, per) => new { Paciente = p, Persona = per })
+                .Where(a => (a.Paciente.NoHistoriaClinica.Contains(NoHistoriaClinica) || NoHistoriaClinica == "-1"))
+                .OrderBy(a => a.Persona.Nombre1)
+                .Select(a => new PacienteCompletoDTO()
+                {
+                    Codigo = a.Paciente.Codigo,
+                    Persona = a.Paciente.Persona,
+                    Nombres = a.Persona.Nombre1 + " " + (a.Persona.Nombre2 ?? ""),
+                    Apellidos = a.Persona.Apellido1 + " " + (a.Persona.Apellido2 ?? ""),
+                    NoHistoriaClinica = a.Paciente.NoHistoriaClinica,
+                    FechaNacimiento = a.Persona.FechaNacimiento,
+                    Sexo = a.Persona.Sexo,
+                    NombrePadre = a.Paciente.NombrePadre,
+                    NombreMadre = a.Paciente.NombreMadre,
+                    LugarNacimiento = a.Paciente.LugarNacimiento,
+                    Archivo_Fisico = a.Paciente.ArchivoFisico,
+                    Nombre_Resposable = a.Paciente.NombreResponsable,
+                    Direccion_Responsable = a.Paciente.DireccionResponsable,
+                    Telefono_Responsable = a.Paciente.TelefonoResponsable
 
+                });
 
+                var pacientes = await query.ToListAsync();
+
+                if(pacientes.Count == 0)
+                {
+                    return NotFound("No se han encontrado pacientes para la NoHistoriaClinica dada.");
+                }
+
+                return pacientes;
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ha ocurrido un error al procesar la solicitud");
+            }
+            
 
         }
 
