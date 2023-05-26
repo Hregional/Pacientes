262d1ec78b19457dbb8e2f77dde2143f985cc7ca
Adolfo Ajucum <martin_aj22@hotmail.com>
2023-05-26 14:59:11 -0600

    feat: swagger auth is not working

--- src/DatosPacientes/Program.cs
+++ src/DatosPacientes/Program.cs
@@ -1,11 +1,19 @@
+using DatosPacientes.Configuration;
 using DatosPacientes.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+
+// Acceder a la configuración de la sección AdminApiConfiguration
+var adminApiConfiguration = builder.Configuration.GetSection(nameof(AdminApiConfiguration)).Get<AdminApiConfiguration>();
+
+builder.Services.AddSingleton(adminApiConfiguration);
+
 
 // Add services to the container.
 
@@ -18,8 +26,41 @@ builder.Services.AddDbContext<RecepcionV2Context>(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add security definition
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.SwaggerDoc(adminApiConfiguration.ApiVersion, new OpenApiInfo { Title = adminApiConfiguration.ApiName, Version = adminApiConfiguration.ApiVersion });
+
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {
+                    AuthorizationUrl = new Uri($"{adminApiConfiguration.IdentityServerBaseUrl}/connect/authorize"),
+                    TokenUrl = new Uri($"{adminApiConfiguration.IdentityServerBaseUrl}/connect/token"),
+                    Scopes = new Dictionary<string, string> {
+                                { adminApiConfiguration.OidcApiName, adminApiConfiguration.ApiName }
+                            }
+                }
+            }
+        });
+        options.OperationFilter<AuthorizeCheckOperationFilter>();
+    });
+
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = adminApiConfiguration.IdentityServerBaseUrl;
+        options.RequireHttpsMetadata = false;
+        options.Audience = adminApiConfiguration.OidcApiName;
+    });
 
 var app = builder.Build();
 
@@ -31,6 +72,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
