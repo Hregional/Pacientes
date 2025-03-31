be82c4974ade094d1475643a7c402ff2976baa59
adolfin <martin_aj22@hotmail.com>
2025-03-31 16:21:33 -0600

    feat: security service added

--- src/DatosPacientes/Program.cs
+++ src/DatosPacientes/Program.cs
@@ -1,13 +1,15 @@
 using DatosPacientes.Configuration;
 using DatosPacientes.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
-    .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+    .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 // Acceder a la configuración de la sección AdminApiConfiguration
 //var adminApiConfiguration = builder.Configuration.GetSection(nameof(AdminApiConfiguration)).Get<AdminApiConfiguration>();
@@ -24,6 +26,20 @@ builder.Services.AddControllers(
 builder.Services.AddDbContext<RecepcionV2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("cnDatabase")));
 
+// Configuración de autenticación con KeyCloak
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "http://localhost:8080/realms/myrealm";
+        options.RequireHttpsMetadata = false;
+        options.Audience = "myapi-client";  // Cambia esto si tu cliente tiene otro nombre
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = true,
+            ValidAudience = "myapi-client"  // Debe coincidir con el audience del token emitido
+        };
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
@@ -31,56 +47,57 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
     options =>
     {
-      // options.SwaggerDoc(adminApiConfiguration.ApiVersion, new OpenApiInfo { Title = adminApiConfiguration.ApiName, Version = adminApiConfiguration.ApiVersion });
-
-       /* options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
             Type = SecuritySchemeType.OAuth2,
             Flows = new OpenApiOAuthFlows
             {
                 AuthorizationCode = new OpenApiOAuthFlow
                 {
-                    AuthorizationUrl = new Uri($"{adminApiConfiguration.IdentityServerBaseUrl}/connect/authorize"),
-                    TokenUrl = new Uri($"{adminApiConfiguration.IdentityServerBaseUrl}/connect/token"),
-                    Scopes = new Dictionary<string, string> {
-                                { adminApiConfiguration.OidcApiName, adminApiConfiguration.ApiName }
-                            }
+                    AuthorizationUrl = new Uri($"{builder.Configuration["Keycloak:Authority"]}/protocol/openid-connect/auth"),
+                    TokenUrl = new Uri($"{builder.Configuration["Keycloak:Authority"]}/protocol/openid-connect/token"),
+                    Scopes = new Dictionary<string, string>
+                {
+                    { "openid", "OpenID Connect" }
+                }
                 }
             }
         });
-        options.OperationFilter<AuthorizeCheckOperationFilter>();
-       */
-    });
-
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-/*
-// Add authentication
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", options =>
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
-        options.Authority = adminApiConfiguration.IdentityServerBaseUrl;
-        options.RequireHttpsMetadata = true;
-        options.Audience = adminApiConfiguration.OidcApiName;
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new[] { "openid" }
+        }
     });
-*/
+    });
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.OAuthClientId("myapi-client");
+            options.OAuthAppName("My API");
+        });
+    }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-//app.UseAuthentication();
-
-//app.UseAuthorization();
-
-app.UseCors(x => x
+    app.UseCors(x => x
     .AllowAnyOrigin()
        .AllowAnyMethod()
           .AllowAnyHeader());
